7d36a900eb7e6e71814c12c53f87d47408a57965
ruiborreicho <rui.borreicho23@gmail.com>
2020-06-12 12:37:58 +0100

    Escrita da Interface

---
+++ projecto/src/projecto/BinTree.java
@@ -0,0 +1,13 @@
+package projecto;
+
+public interface BinTree {
+	
+	public void inserir(int valor);
+	
+	public NoBinTree procurar(int valor);
+	
+	public void remover(int valor);
+	
+	public NoBinTree editarNome(int valor);
+	
+}
